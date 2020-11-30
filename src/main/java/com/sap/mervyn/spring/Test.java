a113403393975daffccacd46a730b26cad9ef7dc
I347764 <mervyn.chen@sap.com>
2020-11-30 15:05:42 +0800

    first test for maven project

--- src/main/java/com/sap/mervyn/spring/Test.java
+++ src/main/java/com/sap/mervyn/spring/Test.java
@@ -3,6 +3,6 @@ package com.sap.mervyn.spring;
 public class Test {
 
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/main/java/com/sap/mervyn/spring/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,9 @@
+package com.sap.mervyn.spring.soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
